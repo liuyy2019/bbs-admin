8bb6e0fc73577fa595ef59c835c9cdeb34b1d3bf
liuyy2019 <2020388406@qq.com>
2021-10-12 22:43:15 +0800

    ADD - 添加拖拽功能

--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -1,11 +1,36 @@
 /* 仪表板可视化显示数据 */
 import React from 'react'
 import {Card,Col, Row ,Statistic,List,Typography} from 'antd';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import { ArrowUpOutlined, ArrowDownOutlined } from '@ant-design/icons';
 import Bar from "../../components/echars/bar";
 import Pie from "../../components/echars/pie";
 import Pie4 from "../../components/echars/pie4";
 import {getListInvitationsByVisitors} from "../../api/index";
+
+const grid = 8;
+// 水平样式
+const getItemStyle = (isDragging, draggableStyle) => ({
+    // some basic styles to make the items look a bit nicer
+    userSelect: 'none',
+    padding: grid,
+    margin: `0 ${grid}px 0 0`,
+
+    // change background colour if dragging
+    background: isDragging ? 'lightgreen' : '#ffffff',
+
+
+    // styles we need to apply on draggables
+    ...draggableStyle,
+});
+const getListStyle = isDraggingOver => ({
+    background: isDraggingOver ? 'lightblue' : '#ffffff',
+    display: 'flex',
+    padding: grid,
+    margin: `0 ${grid}px`,
+    overflow: 'auto',
+    justifyContent: 'space-between',
+});
 
 class Dashboard extends React.Component{
 
@@ -14,26 +39,29 @@ class Dashboard extends React.Component{
         this.state={
             date: new Date().toLocaleString(),
             data: [], /* 帖子数据 */
+            statisticList: [
+                { id: '1', span: 4, title: '用户总数：75', value: '5', valueStyle: { color: '#3f8600' },prefix: <ArrowUpOutlined />},
+                { id: '2', span: 4, title: '登陆用户：10', value: '1', valueStyle: { color: '#cf1322' },prefix: <ArrowDownOutlined />},
+                { id: '3', span: 8, title: '当前时间', value: '5', valueStyle: { color: '#3f8600' }},
+                { id: '4', span: 4, title: '帖子总数：105', value: '5', valueStyle: { color: '#cf1322' },prefix: <ArrowUpOutlined />},
+                { id: '5', span: 4, title: '发帖数量：10', value: '11.28', valueStyle: { color: '#3f8600' },prefix: <ArrowUpOutlined />},
+            ],
         }
     }
 
-    componentWillMount() {
+    componentDidMount() {
         getListInvitationsByVisitors(result => {
             this.setState({
                 data:result
             })
-        })
-    }
-
-    componentDidMount() {
+        });
         this.timer = setInterval(()=>this.tick(),1000)
-    //    参考链接：https://blog.csdn.net/MiemieWan/article/details/79413157
-    //    https://reactjs.bootcss.com/docs/state-and-lifecycle.html
+        //    参考链接：https://blog.csdn.net/MiemieWan/article/details/79413157
+        //    https://reactjs.bootcss.com/docs/state-and-lifecycle.html
     }
 
     componentWillUnmount() {
         clearInterval(this.timer);
-
     }
 
 
@@ -42,77 +70,84 @@ class Dashboard extends React.Component{
             date :  new Date().toLocaleString()
         })
     };
+
+    reOrder = (list, startIndex, endIndex) => {
+        const result = Array.from(list);
+        const [removed] = result.splice(startIndex, 1);
+        result.splice(endIndex, 0, removed);
+        return result;
+    };
+
+    onDragEnd = (result) => {
+        console.log(result)
+        if (!result.destination) {
+            return;
+        }
+
+        const items = this.reOrder(
+            this.state.statisticList,
+            result.source.index,
+            result.destination.index
+        );
+
+        this.setState({
+            statisticList: items
+        });
+    };
+
     render(){
-        /*const data = [
-            'Racing car sprays burning fuel into crowd.',
-            'Japanese princess to wed commoner.',
-            'Australian walks 100km after outback crash.',
-            'Man charged over missing wedding girl.',
-            'Los Angeles battles huge wildfires.',
-            'Los Angeles battles huge wildfires.',
-        ];*/
+        const { statisticList } = this.state;
         return(
-            <div style={{//padding:15,//backgroundImage:`url(${img})`
-                }}
-                 className="site-card-wrapper"
-            >
-                <Row gutter={[16,16]} style={{margin:'10px'}}>
-                    <Col span={4}>
-                        <Card>
-                            <Statistic
-                                title="用户总数：75"
-                                value={5}
-                                precision={2}
-                                valueStyle={{ color: '#3f8600' }}
-                                prefix={<ArrowUpOutlined />}
-                                suffix="%"
-                            />
-                        </Card>
-                    </Col>
-                    <Col span={4}>
-                        <Card>
-                            <Statistic
-                                title="登陆用户：10"
-                                value={1}
-                                precision={2}
-                                valueStyle={{ color: '#cf1322' }}
-                                prefix={<ArrowDownOutlined />}
-                                suffix="%"
-                            />
-                        </Card>
-                    </Col>
-                    <Col span={8}>
-                        <Card>
-                            <h2 style={{textAlign: 'center',margin:'auto'}}>当前时间</h2>
-                            {/*<p style={{textAlign: 'center',fontSize:'18px'}}>{new Date().toLocaleString()}</p>*/}
-                            <p style={{textAlign: 'center',fontSize:'18px'}}>{this.state.date}</p>
-                        </Card>
-                    </Col>
-                    <Col span={4}>
-                        <Card>
-                            <Statistic
-                                title="帖子总数：105"
-                                value={5}
-                                precision={2}
-                                valueStyle={{ color: '#3f8600' }}
-                                prefix={<ArrowUpOutlined />}
-                                suffix="%"
-                            />
-                        </Card>
-                    </Col>
-                    <Col span={4}>
-                        <Card>
-                            <Statistic
-                                title="发帖数量：10"
-                                value={11.28}
-                                precision={2}
-                                valueStyle={{ color: '#3f8600' }}
-                                prefix={<ArrowUpOutlined />}
-                                suffix="%"
-                            />
-                        </Card>
-                    </Col>
-                </Row>
+            <div className="site-card-wrapper">
+                <DragDropContext onDragEnd={this.onDragEnd}>
+                        <Droppable droppableId="droppable" direction="horizontal">
+                            {
+                                (provided, snapshot) => (
+                                    <div ref={provided.innerRef} {...provided.droppableProps}
+                                         style={getListStyle(snapshot.isDraggingOver)}
+                                     >
+                                        {
+                                            statisticList.map((item, index) => (
+                                                <Draggable key={item.id} draggableId={item.id} index={index}>
+                                                    {(provided2, snapshot2) => (
+                                                        <div ref={provided2.innerRef}
+                                                             {...provided2.draggableProps}
+                                                             {...provided2.dragHandleProps}
+                                                             style={getItemStyle(
+                                                                 snapshot2.isDragging,
+                                                                 provided2.draggableProps.style
+                                                             )}
+                                                        >
+                                                            {
+                                                                item.span === 4 ? (
+                                                                    <Card>
+                                                                        <Statistic
+                                                                            title={item.title}
+                                                                            value={item.value}
+                                                                            precision={2}
+                                                                            valueStyle={item.valueStyle}
+                                                                            prefix={item.prefix}
+                                                                            suffix="%"
+                                                                        />
+                                                                    </Card>
+                                                                ) : (
+                                                                    <Card>
+                                                                        <h2 style={{textAlign: 'center',margin:'auto'}}>当前时间</h2>
+                                                                        <p style={{textAlign: 'center',fontSize:'18px'}}>{this.state.date}</p>
+                                                                    </Card>
+                                                                )
+                                                            }
+                                                        </div>
+                                                    )}
+                                                </Draggable>
+                                            ))
+                                        }
+                                        {provided.placeholder}
+                                    </div>
+                                )
+                            }
+                        </Droppable>
+                </DragDropContext>
                 <Row gutter={[16,16]} style={{margin:'10px'}}>
                     <Col span={12}>
                         <Card style={{background:"#00000000"}} >
